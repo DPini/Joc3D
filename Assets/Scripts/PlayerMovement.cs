c52fff6ce7ddabf63980a27566a3202f20d41913
David Pinilla Caparrós <dpinitux@gmail.com>
2017-11-21 21:57:45 +0100

    Added Player Rick and Little Grass Tile. Movement
    Added new models and implemented basic movement for the player
    Changed default camera so it shows the scene from above.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 5.0f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKey(KeyCode.LeftArrow))
+            gameObject.transform.Translate(-speed * Time.deltaTime, 0.0f, 0.0f);
+        else if (Input.GetKey(KeyCode.RightArrow))
+            gameObject.transform.Translate(speed * Time.deltaTime, 0.0f, 0.0f);
+        else if (Input.GetKey(KeyCode.UpArrow))
+            gameObject.transform.Translate(0.0f, 0.0f, speed * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.DownArrow))
+            gameObject.transform.Translate(0.0f, 0.0f, -speed * Time.deltaTime);
+    }
+}
