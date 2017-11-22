f45068f89eeb9205586201c505ab17fde62697fe
JTorras <j.torras95@gmail.com>
2017-11-22 20:22:23 +0100

    generar suelo aleatoriamente (2 zonas por ahora)

---
+++ Assets/Scripts/Level/Floor/FloorLoader.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorLoader : MonoBehaviour
+{
+
+    public GameObject safeFloorInstance;
+    public GameObject roadFloorInstance;
+
+    private int tamFloor = 1;
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < 100; ++i)
+        {
+            CreateRow(i);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //do nothing
+    }
+
+    private void CreateRow(float position) {
+        GameObject floorInstance = GetRandomTile();
+        GameObject obj;
+        for (int i = -10; i < 10; ++i) {
+            obj = Instantiate(floorInstance, new Vector3(i * tamFloor, 0.0f, position * tamFloor), new Quaternion(0.0f, Mathf.PI / 2, 0.0f, 0.0f)) as GameObject;
+            obj.transform.parent = gameObject.transform;
+        } 
+    }
+
+    private GameObject GetRandomTile() {
+        GameObject ret;
+        int rnd = Random.Range(0, 2);
+        Debug.Log("rand: " + rnd);
+        switch (rnd) {
+            case 1:
+                ret = roadFloorInstance;
+                break;
+            default:
+                ret = safeFloorInstance;
+                break;
+        }
+        return ret;
+    }
+}
