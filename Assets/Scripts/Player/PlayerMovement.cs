c56a9adb7447fe107005b447882ff36b30641d19
David Pinilla Caparrós <dpinitux@gmail.com>
2017-12-13 16:28:59 +0100

    Playermovement cleaned

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -14,20 +14,16 @@ public class PlayerMovement : MonoBehaviour {
     public float speed = 5.0f;
 
     public float jump_dist = 1.5f;
-    //public float jump_height = 5.0f;
     public float jump_speed = 1.0f;
 
     public float jump_angle = 45.0f;
 
-    //public float gravity = 9.81f;
     public float gravity = 40.0f;
 
     public float jump_player_scale = 0.8f;
 
     private directions direction = directions.up;
 
-    //private float jump_time;
-    //private float jump_time_count;
     private Vector3 velocity;
     private Vector3 dest_pos;
     private float angular_velocity;
@@ -76,8 +72,6 @@ public class PlayerMovement : MonoBehaviour {
 
         // Based on: https://vilbeyli.github.io/Simple-Trajectory-Motion-Example-Unity3D/
 
-        //jump_time = 0;
-        //current_jump = 0;
 
         dest_pos = transform.position + direction_vector(d)*jump_dist;        
 
@@ -88,42 +82,19 @@ public class PlayerMovement : MonoBehaviour {
 
         float jump_time = Vy * 2 / gravity;
         player_scale_step = ( 1.0f-jump_player_scale )/(jump_time/2);
-        //jump_time_count = 0;
 
 
         angular_velocity = calc_jump_rotation(d) / jump_time;
 
         velocity = ( Vx * direction_vector(d) ) + new Vector3( 0.0f, Vy, 0.0f );
 
-        // Debug.Log("##### JUMPING #####");
-        // Debug.Log("Vi = " + Vi);
-        // Debug.Log("Vy = " + Vx);
-        // Debug.Log("Vx = " + Vy);
-        // Debug.Log("Direction vector = " + direction_vector(d));
-        // Debug.Log("Velocity = " + velocity);
 
         state = states.jumping;
         direction = d;
-        //gameObject.GetComponent<Rigidbody>().isKinematic = false;
 
 
     }
-    /*
-        void jump(){
-            float dir;
-            if (state == states.jumping) dir = 1;
-            else if (state == states.falling) dir = -1;
-            else dir = 0;
-
-            //float y = Mathf.Sin(Time.deltaTime) * dir;
-            float y = Time.deltaTime * dir;
-            gameObject.transform.Translate(0.0f, y, 0.0f);
-            current_jump += y;
-            if (current_jump > jump_height) state = states.falling;
-            jump_time += Time.deltaTime;
-
-        }
-     */
+    
     void physics_update(){
         if ( state == states.jumping ){
             //jump_time_count += Time.deltaTime;
@@ -138,29 +109,12 @@ public class PlayerMovement : MonoBehaviour {
             
         }
     }
-
-    // void OnCollisionEnter(Collision other)
-    // {
-    //     Debug.Log("Collision: " + other.transform.tag);
-    //     if (other.transform.tag == "Ground"){
-    //         Debug.Log("STOPPPPPPPPPPPPPPPPP");
-    //         //gameObject.GetComponent<Rigidbody>().isKinematic = true;
-    //         velocity = Vector3.zero;
-    //         // Place player in precise position.
-    //         transform.position = dest_pos;
-    //         Debug.Log("Position after jumping: " + transform.position);
-    //         state = states.idle;
-    //     }        
-    // }
-
     
     void OnTriggerEnter(Collider other){
         Debug.Log("Collision: " + other.transform.tag);
         if ( state == states.jumping ){
             if (other.transform.tag == "Ground")
             {
-                Debug.Log("STOPPPPPPPPPPPPPPPPP");
-                //gameObject.GetComponent<Rigidbody>().isKinematic = true;
                 velocity = Vector3.zero;
                 angular_velocity = 0;
                 transform.localScale = new Vector3(1,1,1);
@@ -168,7 +122,6 @@ public class PlayerMovement : MonoBehaviour {
                 transform.position = dest_pos;
                 transform.rotation = Quaternion.LookRotation(direction_vector(direction));
 
-                Debug.Log("Position after jumping: " + transform.position);
                 state = states.idle;
             }
         }
@@ -186,19 +139,15 @@ public class PlayerMovement : MonoBehaviour {
             if(Input.GetKey(KeyCode.LeftArrow)){
                 
                 jump(directions.left);
-                //gameObject.transform.Translate(-speed * Time.deltaTime, 0.0f, 0.0f);
             }
             else if (Input.GetKey(KeyCode.RightArrow)){
                 jump(directions.right);
-                //gameObject.transform.Translate(speed * Time.deltaTime, 0.0f, 0.0f);
             }
             else if (Input.GetKey(KeyCode.UpArrow)){
                 jump(directions.up);
-                //gameObject.transform.Translate(0.0f, 0.0f, speed * Time.deltaTime);
             }
             else if (Input.GetKey(KeyCode.DownArrow)){
                 jump(directions.down);
-                //gameObject.transform.Translate(0.0f, 0.0f, -speed * Time.deltaTime);
             }
         }
 
