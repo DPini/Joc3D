9ab6eea0b7eca25aabb918ad21b4af4f23549595
JTorras <j.torras95@gmail.com>
2017-12-13 15:58:39 +0100

    add Controllers

---
+++ Assets/Scripts/Controllers/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    //Floor prefabs
+    public GameObject safeFloorInstance;
+    public GameObject roadFloorInstance;
+
+    private LevelController levelController;
+	// Use this for initialization
+	void Start () {
+        levelController = new LevelController();
+        levelController.Start();
+        levelController.LoadPrefabs(safeFloorInstance, roadFloorInstance);
+        levelController.InitFloor();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/Level/Floor/FloorLoader.cs
+++ Assets/Scripts/Level/Floor/FloorLoader.cs
@@ -2,34 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class FloorLoader : MonoBehaviour
 {
-
+    //Floor prefabs
     public GameObject safeFloorInstance;
     public GameObject roadFloorInstance;
 
     private int nRow;
+    private int nColumns = 40;
 
     private float tamFloor = 1.5f;
-    // Use this for initialization
-    void Start()
+
+    public void LoadPrefabs(GameObject safeFloor, GameObject roadFloor)
     {
+        safeFloorInstance = safeFloor;
+        roadFloorInstance = roadFloor;
+    }
+
+    // Use this for initialization
+    public int[,] InitializeFloor()
+    {
+        int[,] matrixLevel = new int[100,50];
+        int lastZoneUpdated = -4;
+
         nRow = -4;
         int prevZone = -1;
-        while(nRow < 40)
+        while(nRow <= 50)
         {
             int zoneType = GetRandomeZoneInt(prevZone);
-            nRow += createZone(zoneType);
+            int zoneSize = createZone(zoneType);
+            nRow += zoneSize;
+
+            for (int i = lastZoneUpdated; i <= nRow; ++i) {
+                for (int j = 0; j < nColumns; ++j) {
+                    if (i < 0) matrixLevel[100 + i,j] = zoneType;
+                    else matrixLevel[i,j] = zoneType;
+                }
+               
+            }
+
+            lastZoneUpdated += zoneSize;
 
             prevZone = zoneType;
         }
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //do nothing
+        return matrixLevel;
     }
 
     private int createZone(int zoneType) {
@@ -73,7 +91,7 @@ public class FloorLoader : MonoBehaviour
         GameObject obj;
         for (int i = -20; i < 20; ++i) {
             obj = Instantiate(floorInstance, new Vector3(i * tamFloor, 0.0f, position * tamFloor), new Quaternion(0.0f, Mathf.PI / 2, 0.0f, 0.0f)) as GameObject;
-            obj.transform.parent = gameObject.transform;
+            //obj.transform.parent = gameObject.transform;
         } 
     }
 
