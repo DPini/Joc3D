0cd8734dd5f660069a71b591e063c210ca22ffe3
JTorras <joan.torras@vilynx.com>
2018-01-05 03:55:12 +0100

    add bidirectional platforms

--- Assets/Scripts/Controllers/PlatformController.cs
+++ Assets/Scripts/Controllers/PlatformController.cs
@@ -18,13 +18,16 @@ public class PlatformController : MonoBehaviour {
         int row = pos % maxRows;
         float tamPlatform = Random.Range(2, 4);
         float speed = Random.Range(40, 60) / 10.0f;
-
+        int direction = Random.Range(0, 2);
+        if (direction == 0) direction = -1;
         GameObject platformTmp;
         for (int i = 0; i < 6; ++i)
         {
             Destroy(platforms[row, i]);
             platformTmp = Instantiate(platformModel, new Vector3(-8.0f + (2.5f * (tamPlatform * 1.5f) * i), 0.0f, pos * 1.5f), new Quaternion(0.0f, Mathf.PI / 2, 0.0f, 0.0f)) as GameObject;
 
+            Platform platformSettings = platformTmp.GetComponent<Platform>();
+            platformSettings.SetDirection(direction);
 
             platformTmp.transform.localScale = new Vector3(tamPlatform / 10.0f, 0.15f, 0.2f);
             platforms[row, i] = platformTmp;
--- Assets/Scripts/Platform/Platform.cs
+++ Assets/Scripts/Platform/Platform.cs
@@ -6,16 +6,24 @@ public class Platform : MonoBehaviour {
 
     private float speed = 5.0f;
 
+    // 1 -> right
+    // -1 -> left
+    public float direction;
+
     // Use this for initialization
     void Start()
     {
 
     }
 
+    public void SetDirection(int toDirection)
+    {
+        direction = toDirection;
+    }
 
     // Update is called once per frame
     void Update () {
-        float new_x = gameObject.transform.position.x + Time.deltaTime * speed;
+        float new_x = gameObject.transform.position.x + Time.deltaTime * speed * direction ;
         if (new_x < -40) new_x += 70;
         if (new_x > 30) new_x -= 50;
         gameObject.transform.position = new Vector3(new_x, gameObject.transform.position.y, gameObject.transform.position.z);
